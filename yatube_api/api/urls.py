9b77212382e30b9b453e990f7510e3d4ec561fa2
Никита Зайцев <genius.zaytsev@gmail.com>
2023-04-14 16:32:00 +0500

    .

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -12,5 +12,5 @@ router.register('groups', GroupViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/auth/', include('djoser.urls'))
+    path('v1/', include('djoser.urls.jwt'))
 ]
